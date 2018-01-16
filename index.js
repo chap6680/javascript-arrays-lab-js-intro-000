740ab618f2a2c868f81ceb7a4e6704d1acd22b1c
David Chapman <chap6680misc@gmail.com>
2018-01-16 03:00:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const app = "I don't do much."
+  const app = "I don't do much.";
+
+  let kittens = ["Milo", "Otis", "Garfield"];
+//console.log(kittens);
+
+function destructivelyAppendKitten(name) { 
+	kittens.push(name);
+}
+//pends a kitten to the end of the 
+
+/*
+function destructivelyPrependKitten(name) {
+	kittens.unshift(name);
+ }
+
+ //prepends a kitten to the beginning of the kitt
+function destructivelyPrependKitten(name) {
+  
+}
+
+*/
